5029aaf83b7f1837f5c4190868eb02f974d4bce4
Alberto Gutiérrez <39036564+alberto402@users.noreply.github.com>
2020-03-12 17:38:03 +0100

    view and logic Speech to Text and Text to Speech

--- app/src/main/java/com/sample/MovEmt/SpeechViewActivity.java
+++ app/src/main/java/com/sample/MovEmt/SpeechViewActivity.java
@@ -1,14 +1,19 @@
 package com.sample.MovEmt;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,11 +21,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class SpeechViewActivity extends AppCompatActivity {
-    private MediaRecorder grabacion;
-    private String archivoSalida = null;
+    private static final int REQUEST_CODE = 100;
+    private TextView textOutput;
+    private TextToSpeech textToSpeech;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +38,13 @@ public class SpeechViewActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(SpeechViewActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1000);
         }
+        textOutput= (TextView) findViewById(R.id.textOutput);
 
         Button btn_recorder = (Button) findViewById(R.id.recorder);
         btn_recorder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Recorder(v);
-            }
-        });
-
-        Button btn_play = (Button) findViewById(R.id.play);
-        btn_play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play(v);
             }
         });
 
@@ -54,50 +56,73 @@ public class SpeechViewActivity extends AppCompatActivity {
             }
         });
 
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = textToSpeech.setLanguage(Locale.forLanguageTag("es-ES"));
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
     void onClickBackMain(View v){
         Intent intent = new Intent (v.getContext(), MainActivity.class);
         startActivityForResult(intent, 0);
     }
-    public void Recorder(View view){
-        if(grabacion == null){
-            archivoSalida = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Grabacion.mp3";
-            grabacion = new MediaRecorder();
-            grabacion.setAudioSource(MediaRecorder.AudioSource.MIC);
-            grabacion.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            grabacion.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-            grabacion.setOutputFile(archivoSalida);
+    public void Recorder(View view)
 
-            try{
-                grabacion.prepare();
-                grabacion.start();
-            } catch (IOException e){
-            }
-            Toast.makeText(getApplicationContext(), "Grabando...", Toast.LENGTH_SHORT).show();
-        } else if(grabacion != null){
-            grabacion.stop();
-            grabacion.release();
-            grabacion = null;
-            Toast.makeText(getApplicationContext(), "Grabación finalizada", Toast.LENGTH_SHORT).show();
-        }
-    }
+//Create an Intent with “RecognizerIntent.ACTION_RECOGNIZE_SPEECH” action//
 
-    public void play(View view) {
-        if (archivoSalida != null) {
-            MediaPlayer mediaPlayer = new MediaPlayer();
+        {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
             try {
-                mediaPlayer.setDataSource(archivoSalida);
-                mediaPlayer.prepare();
-            } catch (IOException e) {
+
+//Start the Activity and wait for the response//
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"es-ES");
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "¿Qué desea realizar?");
+                startActivityForResult(intent, REQUEST_CODE);
+            } catch (ActivityNotFoundException a) {
+
+            }
+    }
+
+    @Override
+
+//Handle the results//
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textOutput.setText(result.get(0));
+                    int speechStatus = textToSpeech.speak(result.get(0), TextToSpeech.QUEUE_FLUSH, null);
+
+                    if (speechStatus == TextToSpeech.ERROR) {
+                        Log.e("TTS", "Error in converting Text to Speech!");
+                    }
+                }
+                break;
             }
 
-            mediaPlayer.start();
-            Toast.makeText(getApplicationContext(), "Reproduciendo audio", Toast.LENGTH_SHORT).show();
         }
-        else
-            Toast.makeText(getApplicationContext(), "No se ha grabado nada", Toast.LENGTH_SHORT).show();
     }
+
 
 
 }
