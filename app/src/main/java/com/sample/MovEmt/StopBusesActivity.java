43dad85d8426a146118244ea05707adedcd15cd8
FRYoussef <f.r.youssef@hotmail.com>
2020-04-12 17:19:59 +0200

    Implemented recycled view

--- app/src/main/java/com/sample/MovEmt/StopBusesActivity.java
+++ app/src/main/java/com/sample/MovEmt/StopBusesActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.sample.MovEmt.busStopItem.BusItem;
+import com.sample.MovEmt.busStopItem.BusItemAdapter;
+
+import java.util.ArrayList;
 
 public class StopBusesActivity extends AppCompatActivity {
     private RecyclerView rvBus;
@@ -14,6 +20,15 @@ public class StopBusesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_buses);
 
+        // remove its just for test
+        ArrayList<BusItem> alBus = new ArrayList<>();
+        alBus.add(new BusItem("82", 2));
+        alBus.add(new BusItem("133", 3));
+        alBus.add(new BusItem("G", 5));
+
         rvBus = findViewById(R.id.rvBus);
+        rvBus.setHasFixedSize(true);
+        rvBus.setLayoutManager(new LinearLayoutManager(this));
+        rvBus.setAdapter(new BusItemAdapter(alBus));
     }
 }
