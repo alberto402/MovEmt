db70266c743d5be6e521ba97bf512021bbac94e1
Rodrigo <rodrigovelada@gmail.com>
2020-04-28 16:22:22 +0200

    Vista seleccionar ruta (sin logica)

--- app/src/main/java/com/sample/MovEmt/SelectRoute.java
+++ app/src/main/java/com/sample/MovEmt/SelectRoute.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -25,17 +26,22 @@ import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class SelectRoute extends AppCompatActivity implements OnMapReadyCallback {
     private boolean mLocationPermissionGranted = false;
     private MapView mMapView;
+    ArrayList<LatLng> listPoints;
 
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private static final int LOCATION_REQUEST = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,7 @@ public class SelectRoute extends AppCompatActivity implements OnMapReadyCallback
         mMapView.onCreate(mapViewBundle);
 
         mMapView.getMapAsync(this);
+        listPoints = new ArrayList<>();
     }
 
     @Override
@@ -93,7 +100,42 @@ public class SelectRoute extends AppCompatActivity implements OnMapReadyCallback
     }
 
     public void  onMapReady(GoogleMap map){
-        map.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+        map.getUiSettings().setZoomControlsEnabled(true);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                //Reset marker when already 2
+                if (listPoints.size() == 2){
+                    listPoints.clear();
+                    map.clear();
+                }
+                //Save first point select
+                listPoints.add(latLng);
+                //Create marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+
+                if (listPoints.size() == 1){
+                    //Add first marker to the map
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                } else {
+                    //Add second marker to the map
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+                map.addMarker(markerOptions);
+
+                if (listPoints.size() == 2){
+                    //Create URL to get request from first marker to second marker
+                    Toast.makeText(getApplicationContext(),"origen: Latitud:"+listPoints.get(0).latitude + " Longitud: "+ listPoints.get(0).longitude, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"destino: Latitud:"+listPoints.get(1).latitude + " Longitud: "+ listPoints.get(1).longitude, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
     @Override
     public void onPause() {
@@ -111,136 +153,6 @@ public class SelectRoute extends AppCompatActivity implements OnMapReadyCallback
         super.onLowMemory();
         mMapView.onLowMemory();
     }
-
-    /*
-    @Nullable
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //mMapView = (MapView)findViewById(R.id.user_list_map);
-        mMapView = (MapView)findViewById(R.id.action_FirstFragment_to_SecondFragment);
-        /*View view  = inflater.inflate(R.layout., container, false);
-        mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
-
-        initUserListRecyclerView();*/
-    /*
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null){
-            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
-        }
-
-        mMapView.onCreate(mapViewBundle);
-        mMapView.getMapAsync(this);
-
-        return mMapView;
-    }*/
-
-
-
-
-
-
-    /*private boolean checkMapServices(){
-        if(isServicesOK()){
-            if(isMapsEnabled()){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void buildAlertMessageNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
-    }
-
-    public boolean isMapsEnabled(){
-        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
-
-        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-            buildAlertMessageNoGps();
-            return false;
-        }
-        return true;
-    }*/
-
-    /*private void getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-        /*if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-            getChatrooms();
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-    }*/
-
-    /*public boolean isServicesOK(){
-        Log.d("SelectRoute", "isServicesOK: checking google services version");
-
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(SelectRoute.this);
-
-        if(available == ConnectionResult.SUCCESS){
-            //everything is fine and the user can make map requests
-            Log.d("SelectRoute", "isServicesOK: Google Play Services is working");
-            return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
-            //an error occured but we can resolve it
-            Log.d("SelectRoute", "isServicesOK: an error occured but we can fix it");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(SelectRoute.this, available, ERROR_DIALOG_REQUEST);
-            dialog.show();
-        }else{
-            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
-        }
-        return false;
-    }*/
-/*
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.d("SelectRoute", "onActivityResult: called.");
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ENABLE_GPS: {
-                if(mLocationPermissionGranted){
-                    getChatrooms();
-                }
-                else{
-                    getLocationPermission();
-                }
-            }
-        }
-
-    }*/
+    
 
 }
